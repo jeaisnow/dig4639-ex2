f0167ef62e0236fa109c540bd0943005400f8374
jeaisnow <knight@ucf.edu>
2021-02-07 13:48:02 -0500

    Created quote app

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # dig4639-ex2
+
+"Learn from the past to create a better future" 
---
+++ index.js
@@ -0,0 +1,5 @@
+let quotes = ["Learn from the past to create a better future", "Just keep swimming - Dory "]
+
+
+let index = Math.floor(Math.random() * 2)
+console.log(quotes[index])
