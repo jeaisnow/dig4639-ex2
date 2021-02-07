b93ff0eb701aaae4dd0b418a12f7d69719cf8958
jeaisnow <knight@ucf.edu>
2021-02-07 13:51:28 -0500

    closes #1

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let quotes = ["Learn from the past to create a better future", "Just keep swimming - Dory "]
+let quotes = ["Learn from the past to create a better future", "Just keep swimming - Dory ", "They say true love's the greatest weapon to win the war caused by pain - Beyonce"]
 
 
-let index = Math.floor(Math.random() * 2)
+let index = Math.floor(Math.random() * 3)
 console.log(quotes[index])
